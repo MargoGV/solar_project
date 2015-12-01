4dddc256a19afa734e57acf4ae6004b939e63bda
MargoGV <MV.Gubina@yandex.ru>
2015-12-01 10:14:37 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -7,12 +7,12 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue
             object_type = line.split()[0].lower()
-            if object_type == "star": 
+            elif object_type == "star": 
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
             object_type = line.split()[0].lower()
-            if object_type == "planet":  
+            elif object_type == "planet":  
                 planet = Planet()
                 parse_planet_parameters(line, planet)
                 objects.append(planet)
@@ -25,7 +25,7 @@ def read_space_objects_data_from_file(input_filename):
 def parse_star_parameters(line, star):
 	A = line.split()
 	star.r= int(A[1])
-	star.color= A[2]
+	star.color=str(A[2])
 	star.m= float(A[3])
 	star.x= float(A[4])
 	star.y= float(A[5])
@@ -36,7 +36,7 @@ def parse_star_parameters(line, star):
 def parse_planet_parameters(line, planet):
 	A = line.split()
 	planet.r= int(A[1])
-	planet.color= A[2]
+	planet.color= str(A[2])
 	planet.m= float(A[3])
 	planet.x= float(A[4])
 	planet.y= float(A[5])
@@ -48,7 +48,7 @@ def parse_planet_parameters(line, planet):
 def write_space_objects_data_to_file(output_filename, space_objects):
     with open(output_filename, 'w') as out_file:
         for obj in objects:
-            print(out_file,(obj.type, obj.r, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))#iDon'tknow,howtoFIXthisBUG,yet.
+            print(out_file,"%s %f %s %f %f %f %f %f" (obj.type, obj.r, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))#iDon'tknow,howtoFIXthisBUG,yet.
 
 
 if __name__ == "__main__":
