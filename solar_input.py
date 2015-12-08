e6bdbc181532fc8b2b5737a7de665c90ce2bfbb2
MargoGV <MV.Gubina@yandex.ru>
2015-12-08 10:37:28 +0300

    Update solar_input.py

--- solar_input.py
+++ solar_input.py
@@ -6,50 +6,52 @@ from solar_objects import Star, Planet
 
 def read_space_objects_data_from_file(input_filename):
 
-
     objects = []
     with open(input_filename) as input_file:
         for line in input_file:
             if len(line.strip()) == 0 or line[0] == '#':
-                continue 
+                continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  
+            if object_type == "star":  # FIXME: я отфиксил
                 star = Star()
-                parse_star_p(line, star)
+                parse_star_parameters(line, star)
                 objects.append(star)
             elif object_type == "planet":
                 planet = Planet()
-                parse_planet_p(line, planet)
+                parse_planet_parameters(line, planet)
                 objects.append(planet)
             else:
                 print("Unknown space object")
 
     return objects
 
-def parse_star_p(line, star):
 
-    p = line.split(" ")
-    if p[0] == "Star":
-        star.R = float(p[1])
-        star.color = str(p[2])
-        star.m = float(p[3])
-        star.x = float(p[4])
-        star.y = float(p[5])
-        star.Vx = float(p[6])
-        star.Vy = float(p[7])
+def parse_star_parameters(line, star):
+
+    parameters = line.split(" ")
+    if parameters[0] == "Star":
+        star.R = float(parameters[1])
+        star.color = str(parameters[2])
+        star.m = float(parameters[3])
+        star.x = float(parameters[4])
+        star.y = float(parameters[5])
+        star.Vx = float(parameters[6])
+        star.Vy = float(parameters[7])
         print(star.x, star.y)
 
-def parse_planet_p(line, planet):
+
+def parse_planet_parameters(line, planet):
 
     parameters = line.split(" ")
     if parameters[0] == "Planet":
-        planet.R = float(p[1])
-        planet.color = str(p[2])
-        planet.m = float(p[3])
-        planet.x = float(p[4])
-        planet.y = float(p[5])
-        planet.Vx = float(p[6])
-        planet.Vy = float(p[7])
+        planet.R = float(parameters[1])
+        planet.color = str(parameters[2])
+        planet.m = float(parameters[3])
+        planet.x = float(parameters[4])
+        planet.y = float(parameters[5])
+        planet.Vx = float(parameters[6])
+        planet.Vy = float(parameters[7])
+
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -57,7 +59,6 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     with open("output_filename", 'w') as out_file:
         for obj in space_objects:
             print(out_file, "%s %f %s %f %f %f %f %f" % (obj.type,obj.R,obj.color, obj.m, obj.x,obj.y,obj.Vx,obj.Vy ))
-           
 
 
 if __name__ == "__main__":
