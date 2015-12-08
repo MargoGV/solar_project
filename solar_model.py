f7f5693ff614939c68ae6a78f98f8a0d7fb60303
MargoGV <MV.Gubina@yandex.ru>
2015-12-08 10:25:46 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -2,15 +2,10 @@
 # license: GPLv3
 
 gravitational_constant = 6.67408E-11
-"""Гравитационная постоянная Ньютона G"""
 
 
 def calculate_force(body, space_objects):
-    """Вычисляет силу, действующую на тело.
-    Параметры:
-    **body** — тело, для которого нужно вычислить дейстующую силу.
-    **space_objects** — список объектов, которые воздействуют на тело.
-    """
+
 
     body.Fx = body.Fy = 0
     for obj in space_objects:
@@ -23,10 +18,7 @@ def calculate_force(body, space_objects):
 
 
 def move_space_object(body, dt):
-    """Перемещает тело в соответствии с действующей на него силой.
-    Параметры:
-    **body** — тело, которое нужно переместить.
-    """
+
 
     ax = body.Fx/body.m
     body.x += body.Vx*dt+ax*dt**2/2 
@@ -39,11 +31,7 @@ def move_space_object(body, dt):
 
 
 def recalculate_space_objects_positions(space_objects, dt):
-    """Пересчитывает координаты объектов.
-    Параметры:
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-    **dt** — шаг по времени
-    """
+
 
     for body in space_objects:
         calculate_force(body, space_objects)
