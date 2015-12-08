7d419137e76b5e8f051971630660148d4bbf3389
MargoGV <MV.Gubina@yandex.ru>
2015-12-08 10:23:35 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -7,9 +7,7 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
@@ -17,35 +15,32 @@ def calculate_force(body, space_objects):
     body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
+            continue  
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
         F=gravitational_constant*obj.m*body.m/r**2
-        body.Fx += F*((obj.x-body.x)/r)
-        body.Fy += F*((obj.y-body.y)/r)
+        body.Fx += F*((obj.x-body.x)/r)   
+        body.Fy += F*((obj.y-body.y)/r)  
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
-
     Параметры:
-
     **body** — тело, которое нужно переместить.
     """
 
     ax = body.Fx/body.m
-    ay = body.Fy/body.m
-    body.x += body.Vx * dt + ax * (dt ** 2) / 2  # fixed
+    body.x += body.Vx*dt+ax*dt**2/2 
     body.Vx += ax*dt
-    body.y += body.Vy * dt + ay * (dt ** 2) / 2
+    ay = body.Fy/body.m
+    body.y += body.Vy*dt+ay*dt**2/2 
     body.Vy += ay*dt
-    # FIXED: done recalculation of y coordinate!
+    status=open('status.txt','a')
+    print( "%s %f %f %f %f" % (body.color,body.x,body.y,body.Vx,body.Vy ), file=status)
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
@@ -54,6 +49,7 @@ def recalculate_space_objects_positions(space_objects, dt):
         calculate_force(body, space_objects)
     for body in space_objects:
         move_space_object(body, dt)
+    
 
 
 if __name__ == "__main__":
